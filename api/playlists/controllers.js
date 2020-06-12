2b111181ed4010cf5de311d75f8273824165042f
Ayse Akyol <59367104+ayseakyol@users.noreply.github.com>
2020-06-12 12:05:26 +0200

    Update controllers.js

--- api/playlists/controllers.js
+++ api/playlists/controllers.js
@@ -22,13 +22,13 @@ const controllers = {
      INNER JOIN Tracks t ON pt.TrackId = t.TrackId
      WHERE  p.PlaylistId = '${id}'`;
 
-    db.all(sql, (err, row) => {
+    db.all(sql, (err, rows) => {
       if (err) {
         res.status(400).json({ error: err.message });
         return;
       }
 
-      res.json(row);
+      res.json(JSON.stringify(rows));
     });
   },
   create: (req, res) => {
